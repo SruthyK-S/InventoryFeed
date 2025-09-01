22a5403d3187b2a60474670064e1228d45af6ff0
Sruthy K S <141996444+SruthyK-S@users.noreply.github.com>
2025-09-01 20:40:54 +0530

    Create ProductApp.java

---
+++ phase3/com/litmus7/inventory/app/ProductApp.java
@@ -0,0 +1,13 @@
+package com.litmus7.inventory.app;
+
+import com.litmus7.inventory.service.FileProcessorManager;
+
+public class ProductApp 
+{
+	public static void main(String args[])
+	{
+		FileProcessorManager processor = new FileProcessorManager();
+        processor.startProcessing();
+	}
+
+}
